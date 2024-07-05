dd7e27349f1fabf20a2104947f033b92df201a42
Robert Tunna <Robert.Tunna@intelliq.com>
2024-07-05 15:13:51 +0100

    Initial Commit for Data and Single Index Example

---
+++ MongoDB.Indexes/MongoDB.SingleIndex/Program.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+try
+{
+    var client = new MongoClient("mongodb://localhost:27017");
+    var database = client.GetDatabase("Ebook");
+    var collection = database.GetCollection<BsonDocument>("PeopleTestData");
+
+    // Create a single field index on the 'surname' field
+    var indexKeysDefinition = Builders<BsonDocument>.IndexKeys.Ascending("surname");
+    collection.Indexes.CreateOne(new CreateIndexModel<BsonDocument>(indexKeysDefinition));
+
+    Console.WriteLine("Index Added Successfully");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Failed {ex.Message}");
+}
+
